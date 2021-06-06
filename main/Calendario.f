2f0ca08c656bb0a679cacf4865f38b819d4eb8c9
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2021-06-06 13:32:51 -0300

    Fortran program to show the current date and time

---
+++ main/Calendario.f
@@ -0,0 +1,13 @@
+C     Este programa muestra la fecha actual
+
+C     ----------------------
+C     Comienzo del programa.
+C     ----------------------
+      PROGRAM CALENDA
+
+C     ----------------------
+C     Escribir los resultados.
+C     ----------------------
+      WRITE(*,*) "La fecha de hoy es:"
+      WRITE(*,*) FDATE()
+      END PROGRAM CALENDA
