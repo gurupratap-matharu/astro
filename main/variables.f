80165fca19e516dd94c000477da7783d902eb71c
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2021-06-12 13:29:48 -0300

    trying out permitted variable names in fortran

---
+++ main/variables.f
@@ -0,0 +1,12 @@
+      PROGRAM VARIABLES
+C     WE DECLARE THE VARIABLES
+      REAL ASO, Veo, kk_1, st uv, computacion
+      ASO = 5
+      Veo = 1
+      kk_1 = 5
+      st uv = 99
+      computacion = 3.14
+
+      WRITE(*, *)   "ASO = ", ASO
+      WRITE(*, *)   "st uv = ", st uv
+      END PROGRAM VARIABLES
