3536cb3c03bd000c10626f57ba506fddbbf973a1
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2021-06-12 13:28:49 -0300

    fortran program to understand exponential notation

---
+++ main/exponential.f
@@ -0,0 +1,17 @@
+C     -------------------------------------------------------------------
+C     Program to understand how to write scientific notations
+C     -------------------------------------------------------------------
+
+      PROGRAM EXPONENTIAL
+C     -------------------------------------------------------------------
+C     Declare variables for the program
+C     -------------------------------------------------------------------
+      REAL T, N
+C     -------------------------------------------------------------------
+C     Take input from user
+C     -------------------------------------------------------------------
+      
+      WRITE(*,*) "Veer enter T AND N: "
+      READ(*,*) T, N
+      WRITE(*,*) "Veer you entered T and N as: ", T, N
+      END PROGRAM EXPONENTIAL
