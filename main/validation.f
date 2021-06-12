e90a371d9b562acfd1709c9a0313404472e5a13d
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2021-06-12 13:29:30 -0300

    verified various types or power and operator precedence

---
+++ main/validation.f
@@ -0,0 +1,23 @@
+      PROGRAM VALIDATION
+C     ----------------------------------
+C     We declare the variables
+C     ----------------------------------
+      
+      REAL A
+      INTEGER I
+
+C     ----------------------------------------
+C     We try to evaluate different expressions
+C     ----------------------------------------
+      A = 19/4 + 5/4
+      I = 10 ** (2./3.)
+
+C     ----------------------------------------
+C     We print the variables to the console
+C     ----------------------------------------
+      
+      WRITE(*, *)   "A: ", A
+      WRITE(*, *)   "I: ", I
+    
+
+      END PROGRAM VALIDATION
